726d0f8c9a25147560a396a1fff81da0736d2ca7
mathyasp <mathyastp@gmail.com>
2024-07-09 08:47:20 -0400

    comment added to test workflow

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+//Comment to test workflow
 app.use(express.json());
 app.use(express.static('public'));
 
